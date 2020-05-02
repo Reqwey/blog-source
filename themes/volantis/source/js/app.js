7dfd056c77318d02e51a92f030c6395ae03df874
Linhk1606 <Linhk1606@users.noreply.github.com>
2020-05-02 13:55:47 +0800

    update all

--- themes/volantis/source/js/app.js
+++ themes/volantis/source/js/app.js
@@ -345,5 +345,25 @@ var customSearch;
 
 })(jQuery);
 
+// checkbox preload
 $("ul>li>div>h3").prepend("<input type='checkbox' class='indeterminate blue'>");
 $(".indeterminate").prop("indeterminate", true);
+
+/*code-block-fullscreen*/
+$("figcaption").click(function () {
+	if (window.CodeBlockFullscreen) {
+		$("#post").removeClass("code-block-fullscreen");
+		$(this).parent().removeClass("code-block-fullscreen");
+		$(this).parent().removeClass("code-block-fullscreen-overflow-auto");
+		$("html").removeClass("code-block-fullscreen-html-scroll");
+		$(".highlight>table .gutter").removeClass("code-block-fullscreen-gutter");
+		window.CodeBlockFullscreen = false
+	} else {
+		$("#post").addClass("code-block-fullscreen");
+		$(this).parent().addClass("code-block-fullscreen");
+		$(this).parent().addClass("code-block-fullscreen-overflow-auto");
+		$("html").addClass("code-block-fullscreen-html-scroll");
+		$(".highlight>table .gutter").addClass("code-block-fullscreen-gutter");
+		window.CodeBlockFullscreen = true
+	}
+});
