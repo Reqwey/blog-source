221ec28c7a25ff953833a49cc6510b5c84a42370
Lin <50829219+Linhk1606@users.noreply.github.com>
2020-05-30 11:54:40 +0800

    Update app.js

--- themes/volantis/source/js/app.js
+++ themes/volantis/source/js/app.js
@@ -352,38 +352,33 @@ $(".indeterminate").prop("indeterminate", true);
 
 document.addEventListener('keydown',
 function(e) {
-    try {
-        e = e || event || window.event;
-        var keycode = e.keyCode || e.which || e.charCode;
-        var ctrlKey = e.ctrlKey || e.metaKey;
-        if (keycode == 123) { //F12
-            e.preventDefault();
-            window.event.returnValue = false;
-        } else if (keycode == 8) {
-            var act = document.activeElement.tagName.toLowerCase();
-            if (act.indexOf("body") != -1 || act.indexOf("html") != -1) {
-                window.history.back();
-                return false;
-            }
-            return true;
-        } else if (ctrlKey && keycode == 83) { //ctrl+S
-            e.preventDefault();
-            window.event.returnValue = false;
-        } else if (ctrlKey && keycode == 87) { //ctrl+W
-            e.preventDefault();
-            window.event.returnValue = false;
-        } else if (ctrlKey && keycode == 107) { //ctrl++
-            e.preventDefault();
-            window.event.returnValue = false;
-        } else if (ctrlKey && keycode == 109) { //ctrl+-
-            e.preventDefault();
-            window.event.returnValue = false;
-        } else if (keycode == 122) { //F11
-            $('#switchFullScreenCloseBtn').trigger("click");
+    e = e || event || window.event;
+    var keycode = e.keyCode || e.which || e.charCode;
+    var ctrlKey = e.ctrlKey || e.metaKey;
+    if (keycode == 123) { //F12
+        e.preventDefault();
+        window.event.returnValue = false;
+    } else if (keycode == 8) {
+        var act = document.activeElement.tagName.toLowerCase();
+        if (act.indexOf("body") != -1 || act.indexOf("html") != -1) {
+            window.history.back();
+            return false;
         }
-
-    } catch(e) {
-        console.error(e);
+        return true;
+    } else if (ctrlKey && keycode == 83) { //ctrl+S
+        e.preventDefault();
+        window.event.returnValue = false;
+    } else if (ctrlKey && keycode == 87) { //ctrl+W
+        e.preventDefault();
+        window.event.returnValue = false;
+    } else if (ctrlKey && keycode == 107) { //ctrl++
+        e.preventDefault();
+        window.event.returnValue = false;
+    } else if (ctrlKey && keycode == 109) { //ctrl+-
+        e.preventDefault();
+        window.event.returnValue = false;
+    } else if (keycode == 122) { //F11
+        $('#switchFullScreenCloseBtn').trigger("click");
     }
 });
 
