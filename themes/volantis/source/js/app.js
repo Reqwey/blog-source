da87237250a1699e842bf17c4d35e6e1da4956e1
Lin <50829219+Linhk1606@users.noreply.github.com>
2020-05-30 11:58:53 +0800

    Update app.js

--- themes/volantis/source/js/app.js
+++ themes/volantis/source/js/app.js
@@ -358,26 +358,32 @@ function(e) {
     if (keycode == 123) { //F12
         e.preventDefault();
         window.event.returnValue = false;
-    } else if (keycode == 8) {
+    }
+    else if (keycode == 8) {
         var act = document.activeElement.tagName.toLowerCase();
         if (act.indexOf("body") != -1 || act.indexOf("html") != -1) {
             window.history.back();
             return false;
         }
         return true;
-    } else if (ctrlKey && keycode == 83) { //ctrl+S
+    }
+    else if (ctrlKey && keycode == 83) { //ctrl+S
         e.preventDefault();
         window.event.returnValue = false;
-    } else if (ctrlKey && keycode == 87) { //ctrl+W
+    }
+    else if (ctrlKey && keycode == 87) { //ctrl+W
         e.preventDefault();
         window.event.returnValue = false;
-    } else if (ctrlKey && keycode == 107) { //ctrl++
+    }
+    else if (ctrlKey && keycode == 107) { //ctrl++
         e.preventDefault();
         window.event.returnValue = false;
-    } else if (ctrlKey && keycode == 109) { //ctrl+-
+    }
+    else if (ctrlKey && keycode == 109) { //ctrl+-
         e.preventDefault();
         window.event.returnValue = false;
-    } else if (keycode == 122) { //F11
+    }
+    else if (keycode == 122) { //F11
         $('#switchFullScreenCloseBtn').trigger("click");
     }
 });
